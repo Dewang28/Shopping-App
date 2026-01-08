ac9ac5ed5128f2ef2f8ca9cfc23da3755c76b222
Dewang28 <dewangbaluni28@gmail.com>
2026-01-08 17:13:33 +0530

    commit

--- client/app/services/api.ts
+++ client/app/services/api.ts
@@ -1,8 +1,23 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem("token");
+
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
