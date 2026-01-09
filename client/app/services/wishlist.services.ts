d87467f38995871d7232f20e5c03ae68146c985a
Dewang28 <dewangbaluni28@gmail.com>
2026-01-09 23:03:35 +0530

    commit

--- client/app/services/wishlist.services.ts
+++ client/app/services/wishlist.services.ts
@@ -1,11 +1,11 @@
 import  api  from "./api"
 
 export const getWishlist = async () => {
-  const res = await api.get("/wishlist")
+  const res = await api.get("/api/wishlist")
   return res.data
 }
 
 export const toggleWishlist = async (productId: string) => {
-  const res = await api.post("/wishlist/toggle", { productId })
+  const res = await api.post("/api/wishlist/toggle", { productId })
   return res.data
 }
